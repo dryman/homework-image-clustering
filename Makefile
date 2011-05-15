dbf6a48cf0fd7cb4e280792965011cb2ed69fe19
dryman <idryman@gmail.com>
2011-05-15 22:03:40 +0800

    add argv for cluster number

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:	*.c
-	cc `pkg-config --cflags --libs opencv` *.c -O3 -std=c99 -Wall
+	cc `pkg-config --cflags --libs opencv` *.c -O3 -std=c99 -Wall -o cluster
 debug:	*.c
 	cc `pkg-config --cflags --libs opencv` *.c -g -std=c99 -Wall
--- cluster.c
+++ cluster.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <highgui.h>
+#include <memory.h>
 #include <cv.h>
 
 typedef struct {
@@ -19,11 +20,11 @@ void kmean (unsigned char* imgHead, int steps, Cluster* clHead, int cl_num){
     int r = (int) img[0];
     int g = (int) img[1];
     int b = (int) img[2];
-    int sum = 0;
+    int sum = INT32_MAX;
     Cluster* cl = NULL;
     for (Cluster* c = clHead; c!=clHead+cl_num; c++){
       int _sum = abs(c->r-r) + abs(c->g-g) + abs(c->b-b);
-      if (_sum > sum) {
+      if (_sum < sum) {
         sum = _sum;
         cl = c;
       }
@@ -46,6 +47,28 @@ void kmean (unsigned char* imgHead, int steps, Cluster* clHead, int cl_num){
   }
 }
 
+void kmeanApply(unsigned char* dst, const unsigned char* src, int steps, Cluster* clHead, int cl_num){
+  for(int i=0;i<steps*3;i++){
+    unsigned char* img = (unsigned char*)(src+i);
+    unsigned char* img_apply = (unsigned char*)(dst+i);
+    int r = (int) img[0];
+    int g = (int) img[1];
+    int b = (int) img[2];
+    int sum = INT32_MAX;
+    Cluster* cl = NULL;
+    for (Cluster* c = clHead; c!=clHead+cl_num; c++){
+      int _sum = abs(c->r-r) + abs(c->g-g) + abs(c->b-b);
+      if (_sum < sum) {
+        sum = _sum;
+        cl = c;
+      }
+    }
+    img_apply[0]=(unsigned char)cl->r;
+    img_apply[1]=(unsigned char)cl->g;
+    img_apply[2]=(unsigned char)cl->b;
+  }
+}
+
 void dumpCluster(Cluster* clHead, int cl_num){
   for (Cluster* c = clHead; c!=clHead+cl_num; c++){
     printf("Cluster [%d]:\n",(int)(c-clHead));
@@ -54,29 +77,65 @@ void dumpCluster(Cluster* clHead, int cl_num){
   printf("\n");
 }
 
+int initCluster(Cluster** cluster, int cl_num){
+  if (cl_num == 0){
+    cl_num = 5;
+    *cluster = (Cluster*)malloc(sizeof(Cluster)*cl_num);
+    for (Cluster* c = *cluster; c!=*cluster+cl_num; c++){
+      c->r=0;
+      c->g=0;
+      c->b=0;
+      c->r_sum=0;
+      c->g_sum=0;
+      c->b_sum=0;
+      c->count=0;
+    }
+    (*cluster)[0].r=255;
+    (*cluster)[1].r=255;
+    (*cluster)[1].g=255;
+    (*cluster)[2].r=100;
+    (*cluster)[2].g=100;
+    (*cluster)[2].b=100;
+    (*cluster)[3].r=255;
+    (*cluster)[3].g=255;
+    (*cluster)[3].b=255;
+    return cl_num;
+  }
+  else {
+    *cluster = (Cluster*)malloc(sizeof(Cluster)*cl_num);
+    for (Cluster* c = *cluster; c!=*cluster+cl_num; c++){
+      c->r=rand() % 256;
+      c->g=rand() % 256;
+      c->b=rand() % 256;
+      c->r_sum=0;
+      c->g_sum=0;
+      c->b_sum=0;
+      c->count=0;
+    }
+    return cl_num;
+  }
+}
+
+
     
 
 int main(int argc, char** argv){
   IplImage* img = cvLoadImage(argv[1],CV_LOAD_IMAGE_COLOR);
+  IplImage* img_apply = cvCreateImage(cvSize(img->width,img->height),8,3);
+  cvNamedWindow("cluster demo",1);
   unsigned char* imgData = (unsigned char*)img->imageData;
-  Cluster cluster[5];
-  for (Cluster* c = cluster; c!=cluster+5; c++){
-    c->r=0;
-    c->g=0;
-    c->b=0;
-    c->r_sum=0;
-    c->g_sum=0;
-    c->b_sum=0;
-    c->count=0;
-  }
-  cluster[0].r=255;
-  cluster[1].g=255;
-  cluster[2].b=255;
-  cluster[3].r=255;
-  cluster[3].g=255;
-  for (int i=0;i<5;i++){
-    kmean(imgData,img->width*img->height,cluster, 5);
+  unsigned char* img_apply_data = (unsigned char*)img_apply->imageData;
+
+  Cluster* cluster=NULL;
+  int cl_num = initCluster(&cluster,atoi(argv[2]));
+  for (int i=0;i<30;i++){
+    kmean(imgData,img->width*img->height,cluster, cl_num);
     dumpCluster(cluster,5);
+    kmeanApply(img_apply_data,imgData,img->width*img->height,cluster,cl_num);
+    cvShowImage("cluster demo",img_apply);
+    cvWaitKey(100);
   }
+  cvWaitKey(0);
+  
   return 0;
 }
